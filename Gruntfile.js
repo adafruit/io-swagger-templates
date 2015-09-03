e8645be70c62398d47146bd71b51c00225b4c938
Todd Treece <toddtreece@gmail.com>
2015-09-03 15:00:32 -0400

    adds commander server dependency & improves grunt server copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         },
         command: [
           'java -jar ~/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i http://io.adafruit.com/api/docs/api.json -o ~/adafruit-io-server -l nodejs -t node-server',
-          'cp node-server/README.md ~/adafruit-io-server'
+          'find . -type f -not -name "node-server/*.mustache" -exec cp "{}" ~/adafruit-io-server \;'
         ].join('&&')
       }
     },
