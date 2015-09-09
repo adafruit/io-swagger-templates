a681f8c62ba6a644e090a8f07eb9cf7220146262
Todd Treece <toddtreece@gmail.com>
2015-09-09 12:13:24 -0400

    switch back to connect (swagger-node-runner is broken)

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,28 +7,24 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    'replace': {
+    'json-replace': {
       inline: {
         files: [
           {
             expand: true,
             flatten: true,
-            src: ['..//adafruit-io-node/server/swagger.yaml'],
+            src: ['../adafruit-io-node/server/swagger.json'],
             dest: '../adafruit-io-node/server/'
           }
         ],
         options: {
-          usePrefix: false,
-          patterns: [
-            {
-              match: 'host: "io.adafruit.com"',
-              replacement: 'host: "localhost"'
-            },
-            {
-              match: /-\s\"https\"\n/g,
-              replacement: ''
-            }
-          ]
+          space: '  ',
+          replace: {
+            "host": "localhost",
+            "schemes": [
+              "http"
+            ]
+          }
         }
       }
     },
@@ -42,14 +38,13 @@ module.exports = function(grunt) {
           'mvn package',
           'cd ..',
           'java -cp swagger-codegen/modules/swagger-codegen-cli/target/lib/*:./node-server/target/* io.swagger.codegen.Codegen -i http://io.adafruit.com/api/docs/api.json -o ~/adafruit-io-node/server -l adafruit.codegen.AdafruitIoServerGenerator',
-          'java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i http://io.adafruit.com/api/docs/api.json -o ~/adafruit-io-node/server -l swagger-yaml',
-          'rm ~/adafruit-io-node/server/README.md'
+          'wget --output-document=../adafruit-io-node/server/swagger.json http://io.adafruit.com/api/docs/api.json'
         ].join('&&')
       }
     }
   });
 
   // Default task.
-  grunt.registerTask('default', ['shell:node_server', 'replace']);
+  grunt.registerTask('default', ['shell:node_server', 'json-replace']);
 
 };
